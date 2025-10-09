9c7432da1d82eb673e1d99d7f469eb58722d834e
brunoramossilva <brunogabriel0900@gmail.com>
2025-10-09 11:39:18 -0300

    refactor(main) app component and update dependencies

--- App.tsx
+++ App.tsx
@@ -3,11 +3,9 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-50">
-      <Text className="text-3xl font-bold text-blue-600 mb-4">SGMP Mobile</Text>
-      <Text className="text-lg text-gray-600">
-        Sistema de SGMP - Configurado!
-      </Text>
+    <View className="flex-1 bg-slate-50 items-center justify-center">
+      <Text className="text-2xl font-bold text-blue-600 mb-4">SGMP Mobile</Text>
+      <Text className="text-lg text-slate-500">Sistema de Gestãoo ✅</Text>
       <StatusBar style="auto" />
     </View>
   );
