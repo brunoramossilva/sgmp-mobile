51e5a56b354db5f6b37698c81b614845466a6284
brunoramossilva <brunogabriel0900@gmail.com>
2025-10-09 10:25:24 -0300

    feat(main): refactor layout and update text content in app component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-slate-50">
+      <Text className="text-3xl font-bold text-blue-600 mb-4">SGMP Mobile</Text>
+      <Text className="text-lg text-gray-600">
+        Sistema de SGMP - Configurado!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
